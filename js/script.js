e7d813c710a45ea3c1297848dff40584eb05e3b0
Davide <ciampad06@gmail.com>
2020-12-10 16:44:57 +0100

    Fine terzo punto INIZIO BONUS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // x creare una lista di cose da fare e mostrarla in pagina con Vue;
 // x aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
-// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
-
+// x cancellare un item dalla lista, al click sulla relativa icona 'X'.
+//- Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera,
+//  dopo aver scritto il testo nel campo di input.
 var app = new Vue(
   {
     el:"#root",
@@ -17,7 +18,13 @@ var app = new Vue(
     methods:{
       addToDo: function (){
         this.list.push(this.newItem);
+      },
+
+      removeToDo: function(index){
+        console.log(index);
+        this.list.splice(index);
       }
+
     }
   }
 )
